3735f53ea5108210a625bf2e4b82eb47e913f71c
wwdfdo <wwdfdo@gmail.com>
2023-02-14 10:48:06 +0530

    added wordbg layout again

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,16 @@
 import React from "react";
 import QuestionSection from "../components/Questions/QuestionSection";
 // import VideoBg from "../components/VideoBg";
-// import Worldbg from "../components/Worldbg";
-import WorldSlider2 from "../components/WorldSlider2";
+import Worldbg from "../components/Worldbg";
+// import WorldSlider2 from "../components/WorldSlider2";
 // import WorldSlider from "../components/WorldSlider";
 
 function HomePage() {
   return (
     <div>
       {/* <VideoBg /> */}
-      {/* <Worldbg /> */}
-      <WorldSlider2 />
+      <Worldbg />
+      {/* <WorldSlider2 /> */}
       {/* <WorldSlider /> */}
       <QuestionSection />
     </div>
