d27982f30f0a0ec29f83a35e70591d55af388464
wwdfdo <wwdfdo@gmail.com>
2023-03-21 14:16:47 +0530

    added new styles to question

--- src/components/Questions/QuestionSection.jsx
+++ src/components/Questions/QuestionSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useRef, useEffect } from "react";
 import { QuestionArray } from "../../arrays/questionsArray";
 import QsectionTitle from "./QsectionTitle";
 import QsectionFooter from "./QsectionFooter";
@@ -6,9 +7,34 @@ import QsectionAccordion from "./QsectionAccordion";
 import "./QuestionAccordion.css";
 
 const QuestionSection = () => {
+  const componentRef = useRef(null);
+  const [animation, setAnimation] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setAnimation(true);
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    observer.observe(componentRef.current);
+
+    return () => {
+      observer.unobserve(componentRef.current);
+    };
+  }, [componentRef]);
+
   return (
     <div className=" lg:pt-[80px] lg:pb-20   ">
-      <div className="lg:bg-[#737b7b] lg:bg-opacity-20 flex flex-col lg:w-3/4 w-[95%] mx-auto items-center gap-5 lg:py-20 py-10 lg:rounded-3xl">
+      <div
+        ref={componentRef}
+        className={`lg:bg-black lg:bg-opacity-[15%]    flex flex-col lg:w-3/4 w-[95%] mx-auto items-center gap-5 lg:py-20 py-10 lg:rounded-3xl ${
+          animation ? "questionGlass" : ""
+        }`}
+      >
         <QsectionTitle />
         <QsectionAccordion QuestionArray={QuestionArray} />
         <QsectionFooter />
