0aee41a6f33c621f4cf55ea12c1c65e9b1d8361d
wwdfdo <wwdfdo@gmail.com>
2023-04-07 12:25:18 +0530

    added new bg to roadmap

--- src/components/Roadmap/Roadmap.js
+++ src/components/Roadmap/Roadmap.js
@@ -9,7 +9,7 @@ import { roadmapItems } from "../../arrays/roadmapArray";
 
 const Roadmap = () => {
   return (
-    <div className="lg:pt-40 pt-20 lg:pb-20 pb-5 lg:bg-roadmap-bg bg-roadmap-bg   bg-top bg-cover bg-no-repeat bg-blend-darken bg-[#eb3c27] bg-opacity-60  bg-fixed relative">
+    <div className="lg:pt-40 pt-20 lg:pb-20 pb-5 lg:bg-roadmap-bg bg-roadmap-bg   bg-top bg-cover bg-no-repeat bg-blend-darken bg-[#eb3c27] bg-opacity-50  bg-fixed relative">
       <h2 className="text-center lg:text-6xl text-4xl  lg:mb-20 mb-10">
         Road Map
       </h2>
